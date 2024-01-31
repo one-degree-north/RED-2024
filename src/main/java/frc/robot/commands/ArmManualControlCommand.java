4f00a80402ab3cb934a1dc0102725947fe3b576d
marcus <32217062+marcmchin@users.noreply.github.com>
2024-02-01 00:27:25 +0800

    Arm and elevator manual control commands

---
+++ src/main/java/frc/robot/commands/ArmManualControlCommand.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevatarm;
+
+public class ArmManualControlCommand extends Command {
+  private Elevatarm s_Elevatarm;
+  private double m_percent;
+
+  /** Creates a new ArmManualControlCommand. */
+  public ArmManualControlCommand(double percent, Elevatarm elevatarm) {
+    this.s_Elevatarm = elevatarm;
+    this.m_percent = percent;
+    addRequirements(s_Elevatarm);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    s_Elevatarm.setArmPercent(m_percent);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    s_Elevatarm.setArmPosition(s_Elevatarm.getArmRotation2d().getRotations());
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
